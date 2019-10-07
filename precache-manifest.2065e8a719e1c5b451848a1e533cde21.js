1a90a2f8307a8cc48b33237c0c6cbdf468c40af5
Weihao Ke <weihaoke13@gmail.com>
2019-10-06 18:37:04 -0700

    Updates

---
+++ precache-manifest.2065e8a719e1c5b451848a1e533cde21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e7e75a6cb0b52ef90af0a1275bfe59",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5614f2b5deb6839f85d6",
+    "url": "/todolist/static/css/main.54e8f66f.chunk.css"
+  },
+  {
+    "revision": "c1c63ddc4c6ff637209d",
+    "url": "/todolist/static/js/2.f85a78f4.chunk.js"
+  },
+  {
+    "revision": "5614f2b5deb6839f85d6",
+    "url": "/todolist/static/js/main.1c8e4b73.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
